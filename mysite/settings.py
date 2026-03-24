731455cbf6cd9b8ced6f334531807f78b8d38b31
John Louie Cleofas <john.louie_cleofas@questronix.com.ph>
2026-03-24 13:15:02 +0800

    Added accounts app for custom user model

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -75,18 +76,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_django_db',
-        'USER': 'test_django_user',
-        'PASSWORD': 'test_django_password',
-        'HOST': '192.168.0.186',
-        'PORT': '5432',
-    }
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'test_django_db',
+    #     'USER': 'test_django_user',
+    #     'PASSWORD': 'test_django_password',
+    #     'HOST': 'docker',
+    #     'PORT': '5432',
+    # }
 }
 
 
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
